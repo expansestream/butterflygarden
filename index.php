1b5641f9cb81e7146f36298edb82ad5b0ec69709
PlantMan <147006875+expansestream@users.noreply.github.com>
2024-11-11 17:15:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Example 4-28a.php</title>
+   </head>
+      <style>
+         /*CSS Style Here*/
+      </style>
+   <body>
+   <!---HTML Statements Here--->
+   <?php
+   //$row_styles = array('even', 'odd');
+   //$style_index = 0;
+
+   //<img src="https://softcover.s3.amazonaws.com/636/learn_enough_git/images/figures/celebratory_beverage.jpg" height=100 width=100 alt = "Coffee Break">
+
+   $meals = array('<span style="color: blue; font-size: large">Breakfast</span>' => ['<span style="color: brown; text-align: center; margin-left: 1.25em">Walnut Bun</span>', '<span style="color: brown; margin-left: 1.25em">Coffee</span>', '<span style="margin-left:-5em"><img src="https://softcover.s3.amazonaws.com/636/learn_enough_git/images/figures/celebratory_beverage.jpg" height=12.5 width=12.5 alt = "Coffee Break"></span>'],
+            '<span style="color: blue; font-size: large; margin-left: 1.25em">Lunch</span>' => ['<span style="color: brown; margin-left: 1.25em">Cashew Nuts</span>', '<span style="color: brown; margin-left: 1.25em">White Mushrooms</span>', ''],
+            '<span style="color: blue; font-size: large; margin-left: 1.25em">Snack</span>' => ['<span style="color: brown; margin-left: 1.25em">Dried Mulberries</span>', '<span style="color: brown; margin-left: 1.25em">Salted Sesame Crab</span>', '']);
+
+   /*$meals = array('<span style="color: blue; font-size: large">Breakfast</span>' => ['<span style="color: brown">Walnut Bun</span>', '<span style="color: brown">Coffee</span>']);
+      '<span style="color: blue; font-size: large; margin-left: 1.25em">Lunch</span>' => ['<span style="color: brown">Cashew Nuts</span>', '<span style="color: brown">White Mushrooms</span>'],
+      '<span style="color: blue">Snack</span>' => ['<span style="color: brown">Dried Mulberries</span>', '<span style="color: brown">Salted Sesame Crab</span>']);*/
+   
+   /*print $meals['Breakfast'][0];
+   print $meals['Breakfast'][1];*/
+
+   print "<table>\n";
+   foreach ($meals as $key => $value) {
+      print "<tr><td>$key</td><td>$value[0]</td><td>$value[1]</td><td>$value[2]</td></tr>\n";
+   }
+   print '<table>';
+   
+   /*print "<table>\n";
+   foreach ($meal as $key => $value) {
+   print '<tr class="' . $row_styles[$style_index] . '">';
+    print "<tr><td>$key</td><td>$value</td></tr>\n";
+    // This switches $style_index between 0 and 1
+    $style_index = 1 - $style_index;
+   }
+   print '</table>';*/
+   ?>
+      <script>
+      <!---JavaScript Statements Here--->
+      </script>
+   </body>
+</html>
